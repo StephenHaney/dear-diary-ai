94e3fbe891b377eb17ece515833fea95c0b3da69
Stephen Haney <stephen.haney@gmail.com>
2021-03-26 13:03:42 -0700

    Fix for IP lookup service API changes (#18)
    * Fix for IP api
    
    * resilient to IP service changes

--- src/pages/api/getLocationByIp.ts
+++ src/pages/api/getLocationByIp.ts
@@ -7,7 +7,9 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
   const ipAddress = ipAndTheRest.slice(0, ipAndTheRest.indexOf('\n'));
   // Grab the location, thx keycdn:
   const locationDataUrl = `https://tools.keycdn.com/geo.json?host=${ipAddress}`;
-  const locationData = await (await fetch(locationDataUrl, { method: 'get' })).json();
+  const locationData = await (
+    await fetch(locationDataUrl, { method: 'get', headers: { 'User-Agent': 'keycdn-tools:https://deardiary.ai' } })
+  ).json();
   // Return the location json:
-  res.status(200).json(locationData.data.geo);
+  res.status(200).json(locationData?.data?.geo || { ip: 'not found' });
 };
